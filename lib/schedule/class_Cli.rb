a8235bd0db3e0bb6a099bb6406b4bcbcf70ea0d0
Paul Beckwith <303dog@gmail.com>
2019-09-17 19:59:14 +0000

    beginning progress

--- lib/schedule/class_Cli.rb
+++ lib/schedule/class_Cli.rb
@@ -1,20 +1,50 @@
 #CLI Controller
 
-
-
 class Schedule::Class_Cli
 
-    def greeting
-        puts "Hello, this program will help you find your favorite team's game schedule."
-        list_years
+    def call
+        puts "So you want to know which teams played in the Super Bowl last year? Or maybe you want to know the winning score?"
+        start
     end
+
+    def start
+        puts ""
+        puts "Choose a year from the list below and type it in the terminal to find out!"
+        puts ""
+        list_years
+        #input = gets.strip.to_i
+
+        #print_restaurants(input)
+
+        puts ""
+        puts "What restaurant would you like more information on?"
+        #input = gets.strip
+
+        #restaurant = WorldsBestRestaurants::Restaurant.find(input.to_i)
+
+      #  print_restaurant(restaurant)
+
+        puts ""
+        puts "Do you want to see who the champions were for another year ? Enter Y or N"
+
+        input = gets.strip.downcase
+          if input == "y"
+            start
+          elsif input == "n"
+            puts ""
+            puts "Thank you! Have a great day!"
+            exit
+          else
+            puts ""
+            puts "Wait, what did you put?"
+            start
+          end
+      end
 
     def list_years
         puts [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009]
         puts [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
     end
 
-    def user_input
 
-    end
 end
--- lib/schedule/scraper.rb
+++ lib/schedule/scraper.rb
@@ -1,11 +1,14 @@
-require "pry"
-require_relative "nokogiri"
+require 'pry'
+require_relative 'nokogiri'
+require_relative 'open-uri'
 
-class Scraper
+class Schedule::Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions"))
     binding.pry
   end
+
+
 
 end
