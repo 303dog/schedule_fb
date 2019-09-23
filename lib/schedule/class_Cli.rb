1e5f0dfb0ee1693edfe95b7b02f0483467c661e4
Paul Beckwith <303dog@gmail.com>
2019-09-23 13:23:29 -0700

    progress on flow, almost need scraper

--- lib/schedule/class_Cli.rb
+++ lib/schedule/class_Cli.rb
@@ -1,9 +1,8 @@
 #CLI Controller
-
 class Schedule::Class_Cli
 
     def call
-        #Schedule::Scraper.new.discover_year
+        #Schedule::Scraper.new.get_page
         puts "So you want to know which teams played in the Super Bowl last year? Or maybe you want to know the winning score?"
         start
     end
@@ -17,24 +16,34 @@ class Schedule::Class_Cli
         puts "type the year below"
         puts ""
         input = gets.strip.to_i
-        #binding.pry
-        #input = gets.strip.downcase
-       #  if input == "[2000]"
+        
+        
+      
+        if 2000.upto(2019).include?(input)
+            puts "it works!!"
+        else
+          puts "I dont know that.. Please pick a year from the list"
+          puts ""
+          list_years
+        end
 
-#puts "coorosponding years score and teams"
-#binding.pry
-#else #input  # i != "[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]"
-#puts "Nope. That was not an option, pick again!"
-#            input = gets.strip.downcase
-#            list_years
-#          end
-#
-        championships = Schedule::Scraper.find(input.to_i)
+
+
+
+        #input = gets.strip.downcase
+        #  if input
+
+        #   binding.pry
+         puts "coorosponding years score and teams"
+         
+       
+
+        #championships = Schedule::Scraper.find(input.to_i)
 
       #  print_restaurant(restaurant)
 
-      #  puts ""
-      #  puts "Do you want to see who the champions were for another year ? Enter Y or N"
+       #puts ""
+       puts "Do you want to see who the champions were for another year ? Enter Y or N"
 
         input = gets.strip.downcase
           if input == "y"
--- lib/schedule/scraper.rb
+++ lib/schedule/scraper.rb
@@ -5,6 +5,7 @@ require_relative 'open-uri'
  class Schedule::Scraper
 #
    def get_page
+    binding.pry
      doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions"))
      binding.pry
    end
@@ -13,10 +14,11 @@ require_relative 'open-uri'
 #    self.get_page.css("div#t1-50 li")
 #   end
 #
-#   def discover_year
+   def discover_year
+    binding.pry
 #     scrape_games_index.each do |i|
 #     Schedule::Champions.new_from_index_page(i)
-#   end
+   end
 # end
 #
  end
